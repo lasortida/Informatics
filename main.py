492b9cd2b504a060bec186c13740e2cd133620f8
dan <tkdany@yandex.ru>
2022-03-05 10:12:21 +0300

    initial

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import csv
 
-count = 0
-summ = 0
-specCount = 0
-
+main = []
 with open("task.csv", newline='') as file:
     reader = csv.DictReader(file, delimiter=";")
     for row in reader:
         if row['предмет'] == 'информатика' and int(row['балл']) > 600:
-            count += 1
-        if row['предмет'] == 'информатика':
-            specCount += 1
-            summ += int(row['балл'])
-print(count)
-print(summ / specCount)
+            main.append(row)
+
+fileIn = open("input.txt", "w")
+fieldnames = ["округ", "фамилия", "предмет", "балл"]
+writer = csv.DictWriter(fileIn, fieldnames=fieldnames, delimiter=";")
+writer.writeheader()
+writer.writerows(main)

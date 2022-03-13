6d766815699656ab6eaf0533b39b6d90f94be06b
superuser <tkdany@yandex.ru>
2022-03-13 19:37:15 +0300

    new python file

---
+++ bite-operations/byte.py
@@ -0,0 +1,17 @@
+n = int(input())
+k = int(input())
+
+# print(n & ~((1 << k) - 1))
+
+# print(n | (1 << k))
+
+# print(n ^ (1 << k))
+
+# mask = 1 << k
+# new_number = mask & n
+# if (new_number == mask):
+#     print(1)
+# else:
+#     print(0)
+
+# print(n & ~(1 << k))

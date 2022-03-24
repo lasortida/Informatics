014748cf0e24b3f1ad3d4e89e342ce699e5753ae
superuser <tkdany@yandex.ru>
2022-03-24 07:12:32 +0300

    new

--- kege/ege8.py
+++ kege/ege8.py
@@ -9,10 +9,26 @@ import itertools
 # print(globalCount)
 
 
+# globalCount = 0
+# a = itertools.product("123456789", repeat=6)
+# lst = list(map("".join, a))
+# for row in lst:
+#     if row[0] != "5" and row.count("2") == 2:
+#         globalCount += 1
+# print(globalCount)
+
 globalCount = 0
-a = itertools.product("123456789", repeat=6)
+a = itertools.product("012345", repeat=5)
 lst = list(map("".join, a))
 for row in lst:
-    if row[0] != "5" and row.count("2") == 2:
-        globalCount += 1
+    if row[0] != "0":
+        flag = True
+        for i in range(1, len(row)):
+            if row[i] in "024" and row[i - 1] in "024" or row[i] in "135" and row[i - 1] in "135":
+                flag = False
+                break
+        if (flag):
+            globalCount += 1
+            print(row)
 print(globalCount)
+
